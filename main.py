d53318ec18d15193e7a88f1714f989fa2af0b472
princesingh <prince@gmail.com>
2022-08-26 14:38:22 +0530

    Two options(Sign up, Log Out) added in Admin Section

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from time import sleep
 from tabulate import tabulate
 from colorama import init, Fore
 init()
@@ -9,7 +10,7 @@ feature.clr_scrren()
 
 def main_section():
     print("\nAre You?")
-    print(tabulate([["1 - Admin"], ["2 - Student"]], tablefmt="grid"))
+    print(tabulate([["1", "Admin"], ["2", "Student"]], tablefmt="grid"))
 
 
 while True:
@@ -31,7 +32,29 @@ while True:
             temp = True
 
     if option == str(1):
-        print("Admin section feature will be added soon")
+        while True:
+            print(tabulate([["1", "Sign Up"], ["6", "Log Out"]], tablefmt="grid"))
+            while True:
+                try:
+                    admin_option = int(input("Option -> "))
+                    break
+                except:
+                    print(Fore.RED + "Maybe you entered something wrong. Choose right one" + Fore.RESET)
+
+            if admin_option == 1:
+                pass
+            elif admin_option == 6:
+                sleep(2.00)
+                print(Fore.GREEN + "\nYou have been logged out " + Fore.RESET, end="")
+                for _ in range(3):
+                    sleep(1.00)
+                    print(Fore.GREEN + "." + Fore.RESET, end="")
+                feature.clr_scrren()
+                break
+            else:
+                print(Fore.RED + "You choose invalid option. Choose right one" + Fore.RESET)
+
+
     elif option == str(2):
         print("Student scetion feature will be added soon")
     
