11dcd0490e9762da92440d61bf5eae66f0fd42e5
princesingh <prince@gmail.com>
2022-08-27 11:17:59 +0530

    Sign Up code defined in seperate UserManager.py file

---
+++ UserManager.py
@@ -0,0 +1,76 @@
+from validations import Validation
+from curDate_curTime import curDate
+from colorama import init, Fore
+init()
+
+# Section : Admin/Sign Up code
+class UserManager:
+    def SignUp():
+
+        print("~" * 41)
+        print("|"+"\tNew Student (Sign Up)\t\t"+"|")
+        print("~" * 41)
+        print()
+
+        # Section (1): Student Full Name
+        while True:
+            student_name = input("  Student's Name\t:   ")
+            
+            # using if..else block to check Student Name valid or not
+            if Validation.checkStudentName(student_name):
+                break
+            else:
+                print(Fore.RED + "\n  Invalid Student's Name. Try again!\n" + Fore.RESET)
+
+        # Section (2): Student Father's Name
+        while True:
+            father_name = input("  Father's Name\t\t:   ")
+        
+            # using if..else block to check Student Father Name is valid or not 
+            if Validation.checkFatherName(father_name):
+                break
+            else:
+                print(Fore.RED + "\n  Invalid Father's Name. Try Again!\n" + Fore.RESET)
+
+        # Section (3): Student Date of Birth / Format : DD/MM/YYYY / [01/01/1985 to 31/12/2004]
+        while True:
+            student_dob = input("  DOB [dd/mm/yyyy]\t:   ")
+            
+            # using if..else block to check DOB is valid or not
+            if Validation.checkDateofBirth(student_dob):
+                break
+            else:
+                print(Fore.RED + "\n  DOB should be between [01/01/1985 to 31/12/2004]. Try Again!\n" + Fore.RESET)
+
+        # Section (4): Student Mobile Number
+        while True:
+            mobile_no = input("  Mobile No.\t\t:   ")
+            
+            # using if..else block to check Student Mobile Number is valid or not
+            if Validation.checkMobileNo(mobile_no):
+                break
+            else:
+                print(Fore.RED + "\n  Invalid Mobile No. Try Again!\n" + Fore.RESET)
+
+        # Section (5): Student Adhaar Card Number
+        while True:
+            adhaar_no = input("  Adhaar No.\t\t:   ")
+            
+            # using if..else block to check Student Adhaar Number is valid or not
+            if Validation.checkAdhaarNo(adhaar_no):
+                break
+            else:
+                print(Fore.RED + "\n  You entered invalid adhaar no. Try Again!\n" + Fore.RESET)
+
+        current_date = curDate
+        
+        return (student_name, father_name, student_dob, mobile_no, adhaar_no, current_date)
+        
+            
+
+        
+
+
+
+
+
