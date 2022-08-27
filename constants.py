7df5500c904a9a25e55e95550006191577eee540
princesingh <prince@gmail.com>
2022-08-27 11:09:35 +0530

    Some more regular expressions added

--- constants.py
+++ constants.py
@@ -1,2 +1,12 @@
 USER_INPUT_CHECK_USING_REGEX = "(1|2)"
 
+STUDENT_NAME_REGEX = "[A-Za-z]{2,15}( [A-Za-z]{2,15})?( [A-Za-z]{2,15})?"
+
+FATHER_NAME_REGEX = "[A-Za-z]{2,15}( [A-Za-z]{2,15})?( [A-Za-z]{2,15})?"
+
+STUDENT_DOB_REGEX = "(0[1-9]|1[0-9]|2[0-9]|3[01])/(0[1-9]|1[012])/(19(8[5-9]|9[0-9])|200[0-4])"
+
+STUDENT_MOBILE_NO_REGEX =  "[6-9][0-9]{9}"
+
+STUDENT_ADHAAR_NO_REGEX = "[4-9][0-9]{11}"
+
